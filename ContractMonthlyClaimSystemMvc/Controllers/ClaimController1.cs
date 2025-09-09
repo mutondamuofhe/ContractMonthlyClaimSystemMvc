0324e13beadd901d0af2f7782ddd19e5fd8c6068
mutondamuofhe <st10450819@rcconect.edu.za>
2025-09-09 15:48:31 +0000

    Updated some comments

--- ContractMonthlyClaimSystemMvc/Controllers/ClaimController1.cs
+++ ContractMonthlyClaimSystemMvc/Controllers/ClaimController1.cs
@@ -5,13 +5,13 @@ namespace ContractMonthlyClaimSystemMvc.Controllers
 {
     public class ClaimsController : Controller
     {
-        // GET: /Claims/Create
+        // get: /Claims/Create
         public IActionResult Create()
         {
             return View();
         }
 
-        // POST: /Claims/Create
+        // post: /Claims/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
         public IActionResult Create(Claim model)
@@ -21,16 +21,16 @@ namespace ContractMonthlyClaimSystemMvc.Controllers
                 return View(model);
             }
 
-            // TODO: Save the claim to your database or in-memory list
+            // todo: Save the claim to your database or in-memory list
 
             TempData["Success"] = "Claim submitted successfully.";
             return RedirectToAction("Track");
         }
 
-        // GET: /Claims/Track
+        // get: /Claims/Track
         public IActionResult Track()
         {
-            // TODO: return real claims (from DB or service)
+            // todo: return real claims (from DB or service)
             return View();
         }
     }
