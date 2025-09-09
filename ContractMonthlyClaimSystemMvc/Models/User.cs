bb029a6a413b50871361a77b2d4bad8237193d1d
mutondamuofhe <st10450819@rcconect.edu.za>
2025-09-09 15:54:36 +0000

    Added more comments to make it understandable

--- ContractMonthlyClaimSystemMvc/Models/User.cs
+++ ContractMonthlyClaimSystemMvc/Models/User.cs
@@ -4,6 +4,6 @@
     {
         public string Email { get; set; }
         public string Password { get; set; }
-        public string Role { get; set; } // e.g. "Lecturer" or "Manager"
+        public string Role { get; set; } // e.g. "Lecturer" or "Manager" or "program coodinator"
     }
 }
